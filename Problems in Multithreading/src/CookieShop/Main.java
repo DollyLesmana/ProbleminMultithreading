84ba4b9f0718555da92596b390d2e878488fd23a
Dolly Lesmana Lubis <47165953+DollyLesmana@users.noreply.github.com>
2019-02-18 15:35:50 +0700

    Add files via upload

---
+++ Problems in Multithreading/src/CookieShop/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CookieShop;
+
+/**
+ *
+ * @author Dolly Lesmana Lubis
+ */
+public class Main {
+        public static void main (String _args[]){
+            CookieShop shop = new CookieShop(3, 2, 5);
+            shop.startInteracting();
+        }
+    }
---
+++ Problems in Multithreading/src/DiningPhilosopher/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DiningPhilosopher;
+
+/**
+ *
+ * @author Dolly Lesmana Lubis
+ */
+public class Main {
+    public static void main(String _args[]){
+        Table table = new Table(5);
+        table.startDining();
+    }
+}
